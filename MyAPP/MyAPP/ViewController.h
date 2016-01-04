d03ac837f95449b1944d0b834b82233a7cc7be7d
hsh <hsh598600126@163.com>
2016-01-04 18:05:20 +0800

    test test

---
+++ MyAPP/MyAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyAPP
+//
+//  Created by tarena02_01 on 16/1/4.
+//  Copyright © 2016年 tarena02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyAPP/MyAPP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyAPP
+//
+//  Created by tarena02_01 on 16/1/4.
+//  Copyright © 2016年 tarena02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
